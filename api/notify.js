20a1a8396d34929c3c4a18a46d85241901d5cef1
josegonzalez0127u-netizen <josegonzalez0127u@gmail.com>
2026-09-11 22:52:33 -0600

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -2,8 +2,8 @@ const webpush = require('web-push');
 const { Redis } = require('@upstash/redis');
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
 });
 
 module.exports = async function handler(req, res) {
